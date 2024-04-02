52beb7825023bb0f8cd27b6628a9b99019a26425
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-02 20:55:48 -0300

    feat: test deploy

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,14 +1,3 @@
-import app, { init } from "../src/app";
-import dotenv from "dotenv";
-import { logger } from "../src/config";
-import { runCommands } from "../src/helpers";
-dotenv.config();
+import app from "../src/app";
 
-const PORT = process?.env?.PORT || 4000;
-
-init().then(() => {
-  app.listen(PORT, () => {
-    logger.info(`Server is listening on port ${PORT}. Access http://localhost:${PORT}`);
-  });
-  runCommands();
-});
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app, { init } from "./app";
+import dotenv from "dotenv";
+import { logger } from "./config";
+import { runCommands } from "./helpers";
+dotenv.config();
+
+const PORT = process?.env?.PORT || 4000;
+
+init().then(() => {
+  app.listen(PORT, () => {
+    logger.info(`Server is listening on port ${PORT}. Access http://localhost:${PORT}`);
+  });
+  runCommands();
+});
