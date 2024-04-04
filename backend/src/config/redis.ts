6f6eca007345aa35cfebd8581edebfb4714d4035
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-04 19:45:56 -0300

    feat: added catch error on redis connection

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -4,11 +4,15 @@ import { logger } from "./log";
 let redisClient: any;
 
 async function runRedis(): Promise<void> {
-  redisClient = createClient({
-    url: process.env.REDIS_URL
-  });
-  await redisClient.connect();
-  logger.info('redis was connected with success')
+  try {
+    redisClient = createClient({
+      url: process.env.REDIS_URL
+    });
+    await redisClient.connect();
+    logger.info('Redis was connected with success');
+  } catch (error) {
+    logger.error(`Redis was connected with error. Error: ${error}`);
+  }
 }
 runRedis();
 
