681159780272d1be5e85e6c424f216bc1c89c9f8
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-07 20:05:10 -0300

    .

--- backend/src/modules/posts/commands/posts-command.ts
+++ backend/src/modules/posts/commands/posts-command.ts
@@ -4,6 +4,7 @@ import { postsService } from "../services";
 export function runCommandsPosts() {
     setInterval(async () => {
         await postsService.deleteOldPosts();
+        console.log('### deletou posts')
         logger.info(`[COMMANDS - runCommandsPosts] Running posts command, and delete old posts on database`);
     }, (3600 * 100));
 };
--- backend/src/modules/reports/commands/reports-command.ts
+++ backend/src/modules/reports/commands/reports-command.ts
@@ -4,6 +4,7 @@ import reportService from "../services/report-service";
 export function runCommandsReports() {
     setInterval(async () => {
         await reportService.generateReport();
+        console.log('#### atualizou cache')
         logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
     }, (3600 * 100));
 };
