d69f6e5dfdb09b5630fcdbd76a9521391f090d0d
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-07 20:28:22 -0300

    feat: added new lib to jobs

--- backend/src/modules/posts/commands/posts-command.ts
+++ backend/src/modules/posts/commands/posts-command.ts
@@ -1,10 +1,17 @@
 import { logger } from "../../../config";
 import { postsService } from "../services";
+import schedule from 'node-schedule';
 
 export function runCommandsPosts() {
-    setInterval(async () => {
-        await postsService.deleteOldPosts();
-        console.log('### deletou posts')
-        logger.info(`[COMMANDS - runCommandsPosts] Running posts command, and delete old posts on database`);
-    }, (3600 * 1));
+    // setInterval(async () => {
+    //     await postsService.deleteOldPosts();
+    //     console.log('### deletou posts')
+    //     logger.info(`[COMMANDS - runCommandsPosts] Running posts command, and delete old posts on database`);
+    // }, (3600 * 1));
 };
+
+schedule.scheduleJob('* * * * *', async function(){
+    await postsService.deleteOldPosts();
+    console.log('### deletou posts')
+    logger.info(`[COMMANDS - runCommandsPosts] Running posts command, and delete old posts on database`);
+});
--- backend/src/modules/reports/commands/reports-command.ts
+++ backend/src/modules/reports/commands/reports-command.ts
@@ -3,15 +3,16 @@ import reportService from "../services/report-service";
 import schedule from 'node-schedule';
 
 export function runCommandsReports() {
-    setInterval(async () => {
-        await reportService.generateReport();
-        console.log('#### atualizou cache')
-        logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
-    }, (3600 * 1));
+    // setInterval(async () => {
+    //     await reportService.generateReport();
+    //     console.log('#### atualizou cache')
+    //     logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
+    // }, (3600 * 1));
 };
 
-const job = schedule.scheduleJob('* * * * *', function(){
-    console.log('#### The answer to life, the universe, and everything!');
-    logger.info(`###### rodou`);
+schedule.scheduleJob('* * * * *', async function(){
+    await reportService.generateReport();
+    console.log('#### atualizou cache');
+    logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
 });
 
