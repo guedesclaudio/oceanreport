acd91745447696239e6dbf6d2589e431fca7b594
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-07 20:17:03 -0300

    test

--- backend/src/modules/reports/commands/reports-command.ts
+++ backend/src/modules/reports/commands/reports-command.ts
@@ -1,5 +1,6 @@
 import { logger } from "../../../config";
 import reportService from "../services/report-service";
+import schedule from 'node-schedule';
 
 export function runCommandsReports() {
     setInterval(async () => {
@@ -8,3 +9,9 @@ export function runCommandsReports() {
         logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
     }, (3600 * 1));
 };
+
+const job = schedule.scheduleJob('* * * * *', function(){
+    console.log('#### The answer to life, the universe, and everything!');
+    logger.info(`###### rodou`);
+});
+
