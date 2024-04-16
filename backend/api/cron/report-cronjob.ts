5ff2c1d327e289b0194d5a92dcb79c4fc8ab60ae
Claudio Silva <claudio.silva@funcionalcorp.com.br>
2024-04-16 19:47:49 -0300

    test cronjob

--- backend/api/cron/report-cronjob.ts
+++ backend/api/cron/report-cronjob.ts
@@ -2,7 +2,7 @@ import { logger } from '../../src/config'
 import reportService from "../../src/modules/reports/services/report-service";
 import schedule from 'node-schedule';
 
-export async function reportCommand() {
+export async function handler() {
     //schedule.scheduleJob('* * * * *', async function(){
         await reportService.generateReport();
         logger.info(`[COMMANDS - runCommandsReports] Running report command, and update data on cache`);
